036c3cb56b4c203dee5d8c1df8d669160d1cd5f6
Maksym Fedorenko <fedorenko.mx@gmail.com>
2024-01-04 18:11:43 +0200

    Update RedisOptions

--- src/TBot.Core/ConfigureOptions/RedisOptions.cs
+++ src/TBot.Core/ConfigureOptions/RedisOptions.cs
@@ -2,15 +2,23 @@
 
 public class RedisOptions
 {
-    public const string OptionsName = "RedisOptions";
-    
-    public string Host { get; set; } = null!;
-    public string Password { get; set; } = null!;
+    public string? Host { get; set; }
+    public string? Password { get; set; }
     public int DefaultDatabase { get; set; }
     public long SyncTimeout { get; set; }
 
+    private string? _connectionString;
+    public string ConnectionString
+    {
+        get => _connectionString ?? $"{(string.IsNullOrEmpty(Host) ? throw new ArgumentException("Redis Host, cannot be empty!") : Host)}," +
+            $"password={Password}," +
+            $"defaultDatabase={DefaultDatabase}," +
+            $"syncTimeout={SyncTimeout}";
+        set => _connectionString = value;
+    }
+
     public override string ToString()
     {
-        return $"{Host},password={Password},defaultDatabase={DefaultDatabase},syncTimeout={SyncTimeout}";
+        return ConnectionString;
     }
 }
